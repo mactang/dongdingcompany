dd1050468c95e46796358c2d6b19a021b6654dac
gyz <107674510@qq.com>
2015-08-31 17:34:37 +0800

    dfsdfsdfsdf

--- Eoffice1.0/manageAddress/AddressViewController.m
+++ Eoffice1.0/manageAddress/AddressViewController.m
@@ -180,7 +180,6 @@
     [self.navigationController popViewControllerAnimated:YES];
     
 }
-<<<<<<< HEAD:Eoffice1.0/AddressViewController.m
 
 //-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
 //
@@ -206,16 +205,13 @@
     _btnNumber = btn.tag;
     
     NSLog(@"aaa");
-    
-    
-=======
+}
 #pragma mark buttondelegate methds
 -(void)buttondelegate:(UIButton *)button{
     anotherButton.selected  = NO;
     self.signbutton = button.tag-100;
     button.selected = YES;
     anotherButton = button;
->>>>>>> 9d485e7261549113bc728c352968808692aa86c6:Eoffice1.0/manageAddress/AddressViewController.m
 }
 -(void)delegatedata:(NSInteger)buttontag{
     _btnNumber = buttontag;
@@ -362,5 +358,4 @@
     // Pass the selected object to the new view controller.
 }
 */
-
 @end
