570a45f5ee2edd5bd250bfe7f5cd2c3f159c001f
dongdingcompany <358360339@qq.com>
2015-09-22 18:05:16 +0800

    山莨菪碱

---
+++ Eoffice1.0/Classtype/UIAlertViewBlock/UIAlertView+AlerViewBlocks.h
@@ -0,0 +1,31 @@
+//
+//  UIAlertView+AlerViewBlocks.h
+//  AlertViewBlocks
+//
+//  Created by RIMI on 14-7-25.
+//  Copyright (c) 2014年 RIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView (AlerViewBlocks) <UIAlertViewDelegate>
+
+/**
+ *  弹出一个提示框
+ *
+ *  @param title   提示框的标题
+ *  @param msg     描述
+ *  @param confirm 确认按钮标题
+ *  @param cancel  取消按钮标题
+ *  @param blocks  点击按钮会调用此代码块
+ */
++ (void)showMsgWithTitle:(NSString *)title promptmessage:(NSString *)promptmessage confirm:(NSString *)confirm cancel:(NSString *)cancel blocks:(void (^)(NSInteger index))blocks;
+
+
+
+
+
+
+
+
+@end
---
+++ Eoffice1.0/Classtype/UIAlertViewBlock/UIAlertView+AlerViewBlocks.m
@@ -0,0 +1,62 @@
+//
+//  UIAlertView+AlerViewBlocks.m
+//  AlertViewBlocks
+//
+//  Created by RIMI on 14-7-25.
+//  Copyright (c) 2014年 RIMI. All rights reserved.
+//
+
+#import "UIAlertView+AlerViewBlocks.h"
+
+typedef void(^alerBlocks)(NSInteger index);
+
+static alerBlocks _alertViewBlocks;
+
+@implementation UIAlertView (AlerViewBlocks)
+
++ (void)showMsgWithTitle:(NSString *)title
+                     promptmessage:(NSString *)promptmessage
+                 confirm:(NSString *)confirm
+                  cancel:(NSString *)cancel blocks:(void (^)(NSInteger))blocks {
+    
+    _alertViewBlocks = [blocks copy];
+    UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:title message:promptmessage delegate:self cancelButtonTitle:confirm otherButtonTitles:cancel, nil];
+
+//    for (int i=0; i<confirm.count; i++) {
+//        [alertView addButtonWithTitle:confirm[i]];
+//    }
+    [alertView show];
+    
+}
+
++ (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    /**
+     *  调用blocks
+     */
+    _alertViewBlocks(buttonIndex);
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
